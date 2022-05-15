f8176c5664e779486a05600f162fdc092fed4335
ITforAdults <ttymi.kamolov@gmail.com>
2022-05-15 20:05:20 +0200

    first commit

---
+++ app/src/main/java/uz/gita/gravitylabyrint/app/App.kt
@@ -0,0 +1,21 @@
+package uz.gita.gravitylabyrint.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        Timber.plant(Timber.DebugTree())
+    }
+
+    companion object{
+        lateinit var instance: App
+            private set
+    }
+
+}
